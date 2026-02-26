f04d5003f138af1be9a038db794e3ba3bf79e9b6
RajanVermaCG <Rajan.b.verma@capgemini.com>
2026-02-26 16:23:26 +0530

    New Controller Added

--- obj/Debug/net10.0/MyWebApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b024733bbd84f8a895e9bdcbaeebf35ae08a29c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
